848ec7701d513291d9cde3d9e047df9d3424f60e
Soumay Sikchi <120584256+SOUMAYSIKCHI@users.noreply.github.com>
2026-09-15 09:10:07 +0530

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   output: 'static',
-  site: process.env.SITE_URL || 'https://createwithsoumay.store'
+  site: process.env.SITE_URL || 'https://www.createwithsoumay.store'
 });
